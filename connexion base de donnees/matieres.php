96d240ae8023f02faaf73e5da1ba7c3987c8e93d
Amaury Merlin <amaury.merlin@edu.ece.fr>
2023-05-15 00:32:54 +0200

    new push

---
+++ connexion base de donnees/matieres.php
@@ -0,0 +1,16 @@
+<?php require("verification.php"); require("config.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php require("header.php"); ?>
+    <div>Matières<br><?php echo "id_ut : " . $_SESSION['ID_ut'] . "<br>statut : " . $_SESSION['statut'] ?></div>
+</body>
+</html>
+
+<?php require("redirection.php"); mysqli_close($conn); ?>
