117f640b2ef6942d79d3ebb8ccbd5245304d4d5e
Amaury Merlin <amaury.merlin@edu.ece.fr>
2023-05-22 13:35:03 +0200

    nv

--- blocs/config.php
+++ blocs/config.php
@@ -1,15 +1,14 @@
 <?php
+$servername = "localhost";
+$username = "root";
+//$password = "";
+$password = "root";
+$dbname = "omnesmyskills";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
 if ($_SERVER['REQUEST_URI'] == "/projet%20local/config.php") {
     header("Location: accueil.php");
     die();
 }
-$servername = "localhost";
-$username = "root";
-<<<<<<<<< Temporary merge branch 1
-$password = "";
-=========
-$password = "root";
->>>>>>>>> Temporary merge branch 2
-$dbname = "omnesmyskills";
-
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
