fbb71684ca6962351c8a589751f686dff5d90cdd
remii <remi.imbert@edu.ece.fr>
2023-05-23 23:14:16 +0200

    css

--- mescompetences.php
+++ mescompetences.php
@@ -12,27 +12,30 @@ require("blocs/config.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="mesmatieres.css">
 </head>
 <body>
     <?php require("blocs/header.php"); ?>
-    <div><br>Mes compétences<br><br></div>
-    <div>
-        <?php
-        $sql = "SELECT * FROM evaluation WHERE ID_etu LIKE $ID_ut";
-        $result = mysqli_query($conn, $sql);
-        if (mysqli_num_rows($result)){
-            while ($data = mysqli_fetch_assoc($result)){
-                $ID_comp = $data['ID_comp'];
-                $sql2 = "SELECT * FROM competence WHERE ID_comp LIKE $ID_comp";
-                $result2 = mysqli_query($conn, $sql2);
-                if (mysqli_num_rows($result2) == 1){
-                    $data2 = mysqli_fetch_assoc($result2); ?>
-                    <form method="post" action="">
-                        <input type="submit" name="<?php echo $ID_comp ?>" value="<?php echo $data2['nom'] ?>"><br>
-                    </form>
+    <div class="container">
+        <h1>Mes compétences</h1>
+        <div class="Mescompetences">
+            <?php
+            $sql = "SELECT * FROM evaluation WHERE ID_etu LIKE $ID_ut";
+            $result = mysqli_query($conn, $sql);
+            if (mysqli_num_rows($result)){
+                while ($data = mysqli_fetch_assoc($result)){
+                    $ID_comp = $data['ID_comp'];
+                    $sql2 = "SELECT * FROM competence WHERE ID_comp LIKE $ID_comp";
+                    $result2 = mysqli_query($conn, $sql2);
+                    if (mysqli_num_rows($result2) == 1){
+                         $data2 = mysqli_fetch_assoc($result2); ?>
+                        <form method="post" action="">
+                            <input type="submit" class="mescompetences" name="<?php echo $ID_comp ?>" value="<?php echo $data2['nom'] ?>"><br>
+                        </form>
                 <?php }
             }
         } else { echo "Aucune compétence.<br>"; } ?>
+    </div>
     </div>
 </body>
 </html>
